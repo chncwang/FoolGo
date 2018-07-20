1ae3d86fd9820b0ccd6a82d98a07eef07e897a0f
Chauncey Wang <chncwang@gmail.com>
2018-07-20 15:43:04 +0800

    some change

--- src/trainer.cc
+++ src/trainer.cc
@@ -11,6 +11,7 @@
 #include "def.h"
 #include "util/cxxopts.hpp"
 #include "util/SGFParser.h"
+#include "util/rand.h"
 #include "deep_learning/sample.h"
 #include "deep_learning/engine.h"
 #include "deep_learning/cnn/graph_builder.h"
@@ -34,17 +35,24 @@ int main(int argc, char *argv[]) {
   vector<string> strs = parser.chop_all(sgf_file_name);
   cout << strs.size() << endl;
   vector<GameInfo> game_infos = parser.get_game_infos(sgf_file_name);
-  vector<Sample<19>> samples;
 
   int iter = 0;
-  for (const GameInfo &game_info : game_infos) {
-    auto sgf_game = SgfGame<19>::BuildSgfGame(game_info, &samples);
-    sgf_game->Run();
-    if (++iter % 100 == 0) {
-      cout << "iter:" << iter << "sample count:" << samples.size() << endl;
-      break;
-    }
+  cout << "game count:" << game_infos.size() << endl;
+  vector<Sample<19>> samples;
+
+  for (int i=0; i < 1000; ++i) {
+    int rand_game_i = Rand(game_infos.size() - 1, seed);
+    const GameInfo &game_info = game_infos.at(rand_game_i);
+    vector<Sample<19>> single_game_samples;
+    auto game = SgfGame<19>::BuildSgfGame(game_info, &single_game_samples);
+    game->Run();
+    int rand_sample_i = Rand(game_info.moves.size() - 1, seed);
+    samples.push_back(single_game_samples.at(rand_sample_i));
   }
+
+  cout << "samples count:" << samples.size() << endl;
+
+  Engine engine;
 
   return 0;
 }
